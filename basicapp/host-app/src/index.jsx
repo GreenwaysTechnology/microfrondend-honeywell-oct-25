8a20ad7ce5645256420ed641d6da268ac5374b7a
subugee <sasubramanian_md@hotmail.com>
2025-10-27 17:33:28 +0530

    basicapp-host

--- basicapp/host-app/src/index.jsx
+++ basicapp/host-app/src/index.jsx
@@ -2,21 +2,24 @@ import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // dynamic import of a federated module
-const RemoteButton = React.lazy(() => import('remote_app/Button'));
+// const RemoteButton = React.lazy(() => import('remote_app/Button'));
+const RemoteButton = React.lazy(() => import('remote_app/Button').catch(() => {
+    return { default: () => <div>Remote App is Not Available</div> }
+}))
 
 const App = () => {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Host App</h1>
-      <p>This app consumes <strong>Button</strong> exported by remote_app.</p>
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Host App</h1>
+            <p>This app consumes <strong>Button</strong> exported by remote_app.</p>
 
-      <Suspense fallback={<div>Loading remote component...</div>}>
-        <RemoteButton onClick={() => alert('Hello from remote Button inside Host!')}>
-          Federated Button
-        </RemoteButton>
-      </Suspense>
-    </div>
-  );
+            <Suspense fallback={<div>Loading remote component...</div>}>
+                <RemoteButton onClick={() => alert('Hello from remote Button inside Host!')}>
+                    Federated Button
+                </RemoteButton>
+            </Suspense>
+        </div>
+    );
 };
 
 createRoot(document.getElementById('root')).render(<App />);
